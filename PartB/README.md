6d091fa90f9372df56435fe012b1c9eca5250765
Kshetra Hegde <kshetrahegde9@gmail.com>
2022-10-29 00:47:43 -0400

    Second commit

--- PartB/README.md
+++ PartB/README.md
@@ -1,10 +1,8 @@
-
-
 - Kshetra Hegde
 
 A Simple Stop Watch Web Application using the below functions:
 
 - async(): An async function is a function declared with the async keyword,and the await keyword is permitted within it.
-- setInterval():The setInterval() method calls a function at specified intervals (in milliseconds).
-- clearInterval(): The setInterval() method continues calling the function until clearInterval() is called, or the window is closed.
+- setInterval():A method to call a function at specified intervals (in milliseconds).
+- clearInterval(): Method continues calling the function until clearInterval() is called, or the window is closed.
 
--- PartB/script.js
+++ PartB/script.js
@@ -6,4 +6,64 @@ let displaysec1 = 0;
 let displaymin1 = 0;
 let displayhr1 = 0;
 
+let interval = null;
+let status = "stopped";
 
+function stopWatch() {
+  sec1++;
+
+  if (sec1 / 60 === 1) {
+    sec1 = 0;
+    min1++;
+    if (min1 / 60 === 1) {
+      min1 = 0;
+      hr1++;
+    }
+  }
+
+  if (sec1 < 10) {
+    displaysec1 = "0" + sec1.toString();
+  } else {
+    displaysec1 = sec1;
+  }
+  if (min1 < 10) {
+    displaymin1 = "0" + min1.toString();
+  } else {
+    displaymin1 = min1;
+  }
+  if (hr1 < 10) {
+    displayhr1 = "0" + hr1.toString();
+  } else {
+    displayhr1 = hr1;
+  }
+
+  document.getElementById("display").innerHTML =
+    displayhr1 + ":" + displaymin1 + ":" + displaysec1;
+}
+async function delay(ms) {
+  return await new Promise((resolve) => setInterval(resolve, ms));
+}
+
+async function starttimer() {
+  status = "started";
+  while (status !== "stopped") {
+    await delay(1000);
+    stopWatch();
+    document.getElementById("starttimer").innerHTML = "START";
+  }
+}
+
+function stoptimer() {
+  window.clearInterval(interval);
+  document.getElementById("stoptimer").innerHTML = "STOP";
+  status = "stopped";
+}
+
+function reset() {
+  window.clearInterval(interval);
+  sec1 = 0;
+  min1 = 0;
+  hr1 = 0;
+  document.getElementById("display").innerHTML = "00:00:00";
+  document.getElementById("stoptimer").innerHTML = "STOP";
+}
